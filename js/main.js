bce8fa7a70d30a8cd15bc79f95b5b25bf8a6c60a
sergio <khudoleys2@gmail.com>
2020-06-12 01:14:43 +0200

    Agregado JS para el boton volver a inicio. Tambien retocado su estilo

---
+++ js/main.js
@@ -0,0 +1,17 @@
+// Funcion para mostrar u ocultar boton de volver a inicio.
+
+botonInicio = document.getElementById("boton-inicio");
+
+console.log("hola")
+
+var myScrollFunc = function () {
+    var y = window.scrollY;
+    if (y >= 150){
+        botonInicio.style.display = "grid";
+    }
+    else{
+        botonInicio.style.display = "none";
+    }
+}
+
+window.addEventListener("scroll", myScrollFunc);
